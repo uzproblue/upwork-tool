00758f7900142874064d32f01e10c214b025edb7
Firdavs Rakhmonov <48709598+uzproblue@users.noreply.github.com>
2024-01-18 01:13:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { MdOutlineDeleteOutline } from "react-icons/md";
-import { TbCurrencyZloty } from "react-icons/tb";
-import { BsCurrencyDollar } from "react-icons/bs";
 import { PiCurrencyDollarFill } from "react-icons/pi";
 function App() {
   const [input, setInput] = useState();
   const [hourRateInput, setHourRateInput] = useState();
 
   const [expectedAmount, setExpectedAmount] = useState();
-  const [exchangeRate, setExchangeRate] = useState();
   function handleClear() {
     setInput("");
     setExpectedAmount("");
     setHourRateInput("");
   }
-  useEffect(() => {
-    fetch(
-      "https://api.currencyapi.com/v3/latest?apikey=cur_live_oGIs0GaTYjdwJIeot7pXUdd82w0iO1YXoxerVjIF&currencies=PLN"
-    )
-      .then((res) => res.json())
-      .then((res) => setExchangeRate((res?.data?.PLN?.value).toFixed(2)));
-  }, []);
   return (
     <>
       <div className="App">
@@ -54,7 +44,7 @@ function App() {
             value={input}
             onChange={(e) => {
               const value = e.target.value;
-              const payout = value * 0.877;
+              const payout = value * 0.9;
               setInput(value);
               setExpectedAmount(payout.toFixed(2));
             }}
@@ -69,7 +59,7 @@ function App() {
             value={expectedAmount}
             onChange={(e) => {
               const value = e.target.value;
-              const fee = value / 0.877;
+              const fee = value / 0.9;
               setExpectedAmount(value);
               setInput(fee.toFixed(2));
             }}
@@ -79,20 +69,12 @@ function App() {
         <button onClick={() => handleClear()}>
           <MdOutlineDeleteOutline size={24} color="gray" />
         </button>
-        {expectedAmount && exchangeRate && (
-          <>
-            <p className="payment">
-              {expectedAmount} X {exchangeRate + " = "}
-              {(exchangeRate * expectedAmount).toFixed(2)}
-              <TbCurrencyZloty size={36} />
-            </p>
-            <p className="payment bt">
-              {(input * 0.86).toFixed(2)}
-              <BsCurrencyDollar size={28} />
-            </p>
-          </>
-        )}
       </div>
+      {hourRateInput && (
+        <div className="workedfor">
+          Worked for <b>{(input / hourRateInput).toFixed(2)}</b> hours
+        </div>
+      )}
     </>
   );
 }
